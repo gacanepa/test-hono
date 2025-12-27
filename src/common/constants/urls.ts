df368df94be1632b12628285734b1b7ac1719877
Gabriel Cánepa <gacanepa@gmail.com>
2025-12-27 14:12:14 -0300

    Add constants and new weather endpoint

---
+++ src/common/constants/urls.ts
@@ -0,0 +1,2 @@
+export const EXCHANGE_BASE_URL = 'https://dolarapi.com/v1';
+export const WEATHER_BASE_URL = 'https://api.weather.gov';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { EXCHANGE_BASE_URL, WEATHER_BASE_URL } from './common/constants/urls';
 
 const app = new Hono();
 
@@ -19,7 +20,7 @@ app.get('/entry/:id', (c) => {
 
 app.get('/exchange', async (c) => {
   try {
-    const response = await fetch('https://dolarapi.com/v1/cotizaciones');
+    const response = await fetch(`${EXCHANGE_BASE_URL}/cotizaciones`);
     if (!response.ok) {
       return c.json({ error: 'Failed to fetch exchange' }, 500);
     }
@@ -29,6 +30,17 @@ app.get('/exchange', async (c) => {
     const errorMessage = error instanceof Error ? error.message : 'Unknown error';
     return c.json({ error: 'Failed to fetch exchange', details: errorMessage }, 500);
   }
+});
+
+app.get('/weather/:latitude/:longitude', async (c) => {
+  const latitude = c.req.param('latitude');
+  const longitude = c.req.param('longitude');
+  const response = await fetch(`${WEATHER_BASE_URL}/points/${latitude},${longitude}`);
+  if (!response.ok) {
+    return c.json({ error: 'Failed to fetch weather' }, 500);
+  }
+  const data = await response.json();
+  return c.json({ latitude, longitude, data });
 });
 
 export default app;
